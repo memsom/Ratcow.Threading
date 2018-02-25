fa5936e12bec3aa8ea308514fd41096b27660e61
Matt Emson <memson@gmail.com>
2018-02-25 23:27:07 +0000

    initial checkin
    Initial code

---
+++ TaskLockTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TaskLockTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Some vague set of tests
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var t = new Test();
+
+            Console.WriteLine("(1) ----------------------------");
+
+            for (var i = 0; i < 100; i++)
+            {
+                t.Do(i);
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("(2) ----------------------------");
+
+            for (var i = 0; i < 100; i++)
+            {
+                t.Do2(i);
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("(3) ----------------------------");
+
+            for (var i = 0; i < 100; i++)
+            {
+                t.Do3(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TaskLockTest/Test.cs
@@ -0,0 +1,95 @@
+﻿using Ratcow.Threading;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskLockTest
+{
+    internal class Test
+    {
+        AsyncLockObject locker = new AsyncLockObject();
+
+        AsyncLock locker2 = new AsyncLock();
+
+        public void Do(int i)
+        {
+            Task.Factory.StartNew(async () => 
+            {
+                var r = await AsyncMonitor.TryEnter(locker, 20);
+                Console.WriteLine($"before {i} {r}");
+                if (r)
+                {
+                    Console.WriteLine($"locked {i}");
+
+                    try
+                    {
+                        Thread.Sleep(100);
+                    }
+                    finally
+                    {
+                        AsyncMonitor.Exit(locker);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"not locked {i}");
+                }
+
+            });
+        }
+
+        public void Do2(int i)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                var r = await AsyncMonitor.TryEnter(locker2, 20);
+                Console.WriteLine($"before {i} {r}");
+                if (r)
+                {
+                    Console.WriteLine($"locked {i}");
+
+                    try
+                    {
+                        Thread.Sleep(100);
+                    }
+                    finally
+                    {
+                        AsyncMonitor.Exit(locker2);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"not locked {i}");
+                }
+
+            });
+        }
+
+        public void Do3(int i)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                var r = await locker2.Lock(20);
+                Console.WriteLine($"before {i} {r}");
+                if (r)
+                {
+                    Console.WriteLine($"locked {i}");
+
+                    try
+                    {
+                        Thread.Sleep(100);
+                    }
+                    finally
+                    {
+                        locker2.Unlock();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"not locked {i}");
+                }
+
+            });
+        }
+    }
+}
